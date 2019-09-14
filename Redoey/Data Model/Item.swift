7557876cfeb12b19732317e0278a3562c66c0e10
Tim Goens <timcgoens@gmail.com>
2019-09-14 09:53:25 -0500

    refactoring and other things

---
+++ Redoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Redoey
+//
+//  Created by Tim Goens on 9/14/19.
+//  Copyright © 2019 Tim Goens. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
